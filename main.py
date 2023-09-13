5385c33bcf12d5085d2e0d327d652231233aa62c
Christine Marie Huotari <98130829+christinehoot@users.noreply.github.com>
2023-09-13 12:46:48 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
+def get_quests(filepath):
+    with open(filepath, 'r') as file_local:
+        quests_local = file_local.readlines()
+    return quests_local
+
+
+def write_quests(filepath, quests_arg):
+    with open(filepath, 'w') as file:
+        file.writelines(quests_arg)
+
+
+
 while True:
     # Get user input and strip extra space chars and make case insensitive
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip().lower()
 
 
-    if 'add' in user_action or 'new' in user_action:
-         quest = user_action[4:]
+    if user_action.startswith("add"):
+        quest = user_action[4:]
 
-         with open('files/quests.txt', 'r') as file:
-              quests = file.readlines()
+        quests = get_quests('files/quests.txt')
 
-         quests.append(quest)
+        quests.append(quest + '\n')
 
-         with open('files/quests.txt', 'w') as file:
-            file.writelines(quests)
+        write_quests('files/quests.txt', quests)
 
-    elif 'show' in user_action or 'display' in user_action:
+    elif user_action.startswith("show"):
 
-        with open('files/quests.txt', 'r') as file:
-            quests = file.readlines()
+        quests = get_quests('files/quests.txt')
 
 
         for index, item in enumerate(quests):
@@ -30,33 +39,37 @@ while True:
             print(f"You currently have 1 adventure ahead!")
         else:
             print(f"You currently have {len(quests)} adventures ahead!")
-    elif 'edit' in user_action:
-        number = int(user_action[5:])
-        number = number - 1
+    elif user_action.startswith('edit') or user_action.startswith('replace'):
+        try:
+            number = int(user_action[5:])
+            number = number - 1
 
-        with open('files/quests.txt', 'r') as file:
-            quests = file.readlines()
+            quests = get_quests('files/quests.txt')
 
-        new_quest = input("Enter new quest: ")
-        quests[number] = new_quest + "\n"
+            new_quest = input("Enter new quest: ")
+            quests[number] = new_quest + "\n"
 
-        with open('files/quests.txt', 'w') as file:
-            file.writelines(quests)
+            write_quests('files/quests.txt', quests)
+        except ValueError:
+            print("Your command is not valid.")
+            continue
 
-    elif 'complete' in user_action:
-        number = int(user_action[9:])
+    elif user_action.startswith('complete') or user_action.startswith('remove'):
+        try:
+            number = int(user_action[9:])
 
-        with open('files/quests.txt', 'r') as file:
-            quests = file.readlines()
-        index = number - 1
-        quest_to_remove = quests[index].strip('\n')
-        quests.pop(index)
+            quests = get_quests('files/quests.txt')
+            index = number - 1
+            quest_to_remove = quests[index].strip('\n')
+            quests.pop(index)
 
-        with open('files/quests.txt', 'w') as file:
-            file.writelines(quests)
+            write_quests('files/quests.txt', quests)
 
-        message = f"Quest {quest_to_remove} has been removed from the list. Great job, adventurer!"
-        print(message)
+            message = f"Quest {quest_to_remove} has been removed from the list. Great job, adventurer!"
+            print(message)
+        except IndexError:
+            print("There is no item with that number.")
+            continue
 
     elif user_action == 'exit':
         break
